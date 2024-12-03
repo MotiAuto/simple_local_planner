82bacfb4f87cb7a356a50b8be686f5d673f5f98d
motii <108280115+motii8128@users.noreply.github.com>
2024-12-03 10:29:39 +0900

    Add files via upload

---
+++ include/simple_local_planner/simple_local_planner.hpp
@@ -0,0 +1,38 @@
+#ifndef SIMPLE_LOCAL_PLANNER
+#define SIMPLE_LOCAL_PLANNER
+
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <chrono>
+#include <math.h>
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+namespace simple_local_planner
+{
+    class SimpleLocalPlanner : public rclcpp::Node
+    {
+        public:
+        explicit SimpleLocalPlanner(const rclcpp::NodeOptions&option = rclcpp::NodeOptions());
+
+        void topic_callback(const nav_msgs::msg::Path::SharedPtr msg);
+        void timer_callback();
+
+        private:
+        rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub;
+        nav_msgs::msg::Path::SharedPtr path_;
+        uint8_t path_pose_id_;
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub;
+        rclcpp::TimerBase::SharedPtr timer_;
+        std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+        std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    };
+}
+
+#endif
---
+++ src/simple_local_planner.cpp
@@ -0,0 +1,81 @@
+#include "simple_local_planner/simple_local_planner.hpp"
+
+namespace simple_local_planner
+{
+    SimpleLocalPlanner::SimpleLocalPlanner(const rclcpp::NodeOptions& option) : Node("SimpleLocalPlanner", option)
+    {
+        path_sub = this->create_subscription<nav_msgs::msg::Path>(
+            "/path",
+            0,
+            std::bind(&SimpleLocalPlanner::topic_callback, this, _1)
+        );
+
+        timer_ = this->create_wall_timer(1ms, std::bind(&SimpleLocalPlanner::timer_callback, this));
+
+        cmd_pub = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 0);
+
+        tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+        path_pose_id_ = 0;
+    }
+
+    void SimpleLocalPlanner::topic_callback(const nav_msgs::msg::Path::SharedPtr msg)
+    {
+        path_ = msg;
+        path_pose_id_ = 0;
+    }
+
+    void SimpleLocalPlanner::timer_callback()
+    {
+        if(path_ == nullptr || path_->poses.empty())
+        {
+            RCLCPP_WARN(this->get_logger(), "Failed path!!");
+            return;
+        }
+
+        try
+        {
+            geometry_msgs::msg::TransformStamped t = tf_buffer_->lookupTransform("odom", "map", tf2::TimePointZero);
+
+            geometry_msgs::msg::PoseStamped target_pose = path_->poses[path_pose_id_];
+
+            tf2::Vector3 cu_pos, tar_pos;
+            cu_pos.setW(t.transform.rotation.w);
+            cu_pos.setX(t.transform.rotation.x);
+            cu_pos.setY(t.transform.rotation.y);
+            cu_pos.setZ(t.transform.rotation.z);
+            tar_pos.setW(target_pose.pose.orientation.w);
+            tar_pos.setX(target_pose.pose.orientation.x);
+            tar_pos.setY(target_pose.pose.orientation.y);
+            tar_pos.setZ(target_pose.pose.orientation.z);
+
+            auto x_vec = target_pose.pose.position.x - t.transform.translation.x;
+            auto y_vec = target_pose.pose.position.y - t.transform.translation.y;
+            auto delta_degree = tar_pos.getZ() - cu_pos.getZ();
+            auto rotation_vec = delta_degree * (M_PI / 180.0);
+
+            if(x_vec < 0.05 && y_vec < 0.05 && rotation_vec < 0.05)
+            {
+                path_pose_id_ += 1;
+            }
+            else
+            {
+                geometry_msgs::msg::Twist cmd;
+                cmd.linear.x = x_vec;
+                cmd.linear.y = y_vec;
+                cmd.angular.z = rotation_vec;
+
+                cmd_pub->publish(cmd);
+            }
+        }
+        catch(const tf2::TransformException &ex)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Failed to transform : %s", ex.what());
+        }
+        
+    }
+}
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(simple_local_planner::SimpleLocalPlanner)
