4db35391f0aa8519b126909bfb78279a3b5623cb
motii <108280115+motii8128@users.noreply.github.com>
2024-12-03 14:04:03 +0900

    Add files via upload

--- src/simple_local_planner.cpp
+++ src/simple_local_planner.cpp
@@ -12,34 +12,35 @@ namespace simple_local_planner
 
         cmd_pub = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 0);
 
-        tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
-        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+        current_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+            "/current",
+            0,
+            std::bind(&SimpleLocalPlanner::pose_callback, this, _1)
+        );
 
-        path_pose_id_ = 0;
+        t = nullptr;
     }
 
     void SimpleLocalPlanner::topic_callback(const nav_msgs::msg::Path::SharedPtr msg)
     {
         path_ = msg;
 
-        try
+        if(t != nullptr)
         {
-            geometry_msgs::msg::TransformStamped t = tf_buffer_->lookupTransform("map", "odom", tf2::TimePointZero);
-
             geometry_msgs::msg::PoseStamped target_pose = path_->poses.front();
 
             tf2::Vector3 cu_pos, tar_pos;
-            cu_pos.setW(t.transform.rotation.w);
-            cu_pos.setX(t.transform.rotation.x);
-            cu_pos.setY(t.transform.rotation.y);
-            cu_pos.setZ(t.transform.rotation.z);
+            cu_pos.setW(t->pose.orientation.w);
+            cu_pos.setX(t->pose.orientation.x);
+            cu_pos.setY(t->pose.orientation.y);
+            cu_pos.setZ(t->pose.orientation.z);
             tar_pos.setW(target_pose.pose.orientation.w);
             tar_pos.setX(target_pose.pose.orientation.x);
             tar_pos.setY(target_pose.pose.orientation.y);
             tar_pos.setZ(target_pose.pose.orientation.z);
 
-            auto x_vec = target_pose.pose.position.x - t.transform.translation.x;
-            auto y_vec = target_pose.pose.position.y - t.transform.translation.y;
+            auto x_vec = target_pose.pose.position.x - t->pose.position.x;
+            auto y_vec = target_pose.pose.position.y - t->pose.position.y;
             auto delta_degree = tar_pos.getZ() - cu_pos.getZ();
             auto rotation_vec = delta_degree * (M_PI / 180.0);
 
@@ -50,10 +51,11 @@ namespace simple_local_planner
 
             cmd_pub->publish(cmd);
         }
-        catch(const tf2::TransformException &ex)
-        {
-            RCLCPP_ERROR(this->get_logger(), "Failed to transform : %s", ex.what());
-        }
+    }
+
+    void SimpleLocalPlanner::pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+    {
+        t = msg;
     }
 }
 
