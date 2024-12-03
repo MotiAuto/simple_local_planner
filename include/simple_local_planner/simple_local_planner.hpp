70051b862a52087823a13bc00af8770fd3ae88d2
motii <108280115+motii8128@users.noreply.github.com>
2024-12-03 14:06:27 +0900

    Add files via upload

--- include/simple_local_planner/simple_local_planner.hpp
+++ include/simple_local_planner/simple_local_planner.hpp
@@ -4,9 +4,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/path.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <tf2/LinearMath/Quaternion.h>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
 
 #include <chrono>
 #include <math.h>
@@ -22,14 +21,14 @@ namespace simple_local_planner
         explicit SimpleLocalPlanner(const rclcpp::NodeOptions&option = rclcpp::NodeOptions());
 
         void topic_callback(const nav_msgs::msg::Path::SharedPtr msg);
+        void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
 
         private:
         rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr current_sub;
         nav_msgs::msg::Path::SharedPtr path_;
-        uint8_t path_pose_id_;
+        geometry_msgs::msg::PoseStamped::SharedPtr t;
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub;
-        std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-        std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
     };
 }
 
